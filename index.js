f8cea8e0913babb7e03e55c3fdd9a78f1b347e65
Laura Kambanis <laurakambanis@Lauras-MacBook-Air-3.local>
2022-09-24 15:20:25 +0100

    started menu questions

--- index.js
+++ index.js
@@ -4,3 +4,30 @@ const consoleTable = require("console.table");
 const dotenv = require("dotenv");
 
 console.log("./db/schema.sql");
+
+//main menu prompts
+mainMenuPrompt = () => {
+  inquirer
+    .prompt({
+      type: "list",
+      message: "What would you like to do?",
+      choices: [
+        "View all Employees",
+        "Add Employee",
+        "Update Employee Role",
+        "View all Roles",
+        "Add Role",
+        "View All Departments",
+        "Add Department",
+        "Exit",
+      ],
+      name: "mainmenu",
+      default: "use arrow keys",
+    })
+    .then((response) => {});
+  if (response.mainmenu === "View All Employees") {
+  }
+};
+
+//initialises app
+mainMenuPrompt();
