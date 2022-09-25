8fd1ac6b8fd20a20e6e814da1fe7bc6a259bfd6b
Laura Kambanis <laurakambanis@Lauras-Air-3.home>
2022-09-25 17:58:07 +0100

    created initial functions to show tables

--- index.js
+++ index.js
@@ -1,32 +1,116 @@
 const inquirer = require("inquirer");
-const db = require("./db/connection.js");
-const consoleTable = require("console.table");
-const dotenv = require("dotenv");
+const mysql = require("mysql2");
+// const consoleTable = require("console.table");
+
+const db = mysql.createConnection(
+  {
+    host: "localhost",
+    // MySQL username,
+    user: "root",
+    // MySQL password
+    password: "rootroot",
+    database: "myEmployees_db",
+  },
+  console.log(`Connected to the myEmployees_db database.`)
+);
+
+db.connect((err) => {
+  if (err) throw err;
+  console.log("database connected");
+  mainMenuPrompt();
+});
 
 //main menu prompts
-// mainMenuPrompt = () => {
-//   inquirer
-//     .prompt({
-//       type: "list",
-//       message: "What would you like to do?",
-//       choices: [
-//         "View all Employees",
-//         "Add Employee",
-//         "Update Employee Role",
-//         "View all Roles",
-//         "Add Role",
-//         "View All Departments",
-//         "Add Department",
-//         "Exit",
-//       ],
-//       name: "mainmenu",
-//       default: "use arrow keys",
-//     })
-//     .then((response) => {});
-//   if (response.mainmenu === "View All Employees") {
-//     //call a function
-//   }
-// };
+mainMenuPrompt = () => {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "mainmenu",
+        message: "What would you like to do?",
+        choices: [
+          "View All Employees",
+          "Add Employee",
+          "Update Employee Role",
+          "View All Roles",
+          "Add Role",
+          "View All Departments",
+          "Add Department",
+          "Exit",
+        ],
+      },
+    ])
+    .then((response) => {
+      let userChoice = response.choice;
+      console.log(userChoice);
+      switch (userChoice) {
+        case "View All Employees":
+          viewAllEmployees();
+          break;
 
-// //initialises app
-// mainMenuPrompt();
+        case "Add Employee":
+          addEmployee();
+          break;
+
+        case "Update Employee Role":
+          updateRole();
+          break;
+
+        case "View All Roles":
+          viewRoles();
+          break;
+
+        case "Add Role":
+          addRole();
+          break;
+
+        case "View All Departments":
+          viewAllDepartments();
+          break;
+
+        case "Add Department":
+          addDepartment();
+          break;
+      }
+    });
+};
+
+function viewAllEmployees() {
+  db.query("SELECT * FROM employee;", (err, res) => {
+    // if (err) throw err;
+    console.log(res);
+    mainMenuPrompt();
+  });
+}
+
+function viewRoles() {
+  db.query("SELECT * FROM role;", (err, res) => {
+    if (err) throw err;
+    console.log(res);
+    mainMenuPrompt();
+  });
+}
+function viewAllDepartments() {
+  db.query("SELECT * FROM department;", (err, res) => {
+    if (err) throw err;
+    console.log(res);
+    mainMenuPrompt();
+  });
+}
+
+function addEmployee() {
+    inquirer.prompt([
+{
+ type:
+ name:
+ message: 
+
+}
+
+
+    ])
+}
+
+
+//initialises app
+mainMenuPrompt();
